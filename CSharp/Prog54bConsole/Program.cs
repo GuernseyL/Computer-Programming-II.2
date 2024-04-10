a119d08b686255216997048ec623fe4a5c5ec0d5
GuernseyL <154453720+GuernseyL@users.noreply.github.com>
2024-04-10 14:39:52 -0500

    Update 4/10/2024

---
+++ CSharp/Prog54bConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: guernsey.l
+ * Date: 4/10/2024
+ * Time: 2:21 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Prog54bConsole
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Enter num1: ");
+			int num1 = int.Parse(Console.ReadLine());
+			Console.Write("Enter num2: ");
+			int num2 = int.Parse(Console.ReadLine());
+			Console.Write("Enter num3: ");
+			int num3 = int.Parse(Console.ReadLine());
+			Console.Write("Enter num4: ");
+			int num4 = int.Parse(Console.ReadLine());
+			
+			int sum = num1 + num2 + num3 + num4;
+			double average = (double)sum / 4.0;
+			Console.WriteLine("Sum: " + sum);
+			Console.WriteLine("Average: " + Math.Round(average, 2));
+			Console.ReadKey();
+			
+		}
+	}
+}
---
+++ CSharp/Prog54cConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: guernsey.l
+ * Date: 4/10/2024
+ * Time: 2:31 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Prog54cConsole
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Enter radius: ");
+			double radius = int.Parse(Console.ReadLine());
+			
+			double circumfrence = radius * 2;
+			double area = radius * radius * 3.14159;
+			Console.WriteLine("Circumfrence: " + Math.Round(circumfrence, 2));
+			Console.WriteLine("Area: " + Math.Round(area, 2));
+			Console.ReadKey();
+			
+		}
+	}
+}
