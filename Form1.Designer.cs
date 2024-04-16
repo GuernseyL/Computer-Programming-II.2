e54650ec571b542e1e5f080b1b0b93fb099c3311
GuernseyL <154453720+GuernseyL@users.noreply.github.com>
2024-04-16 14:33:12 -0500

    Update 4/16/2024

---
+++ Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace Prog88aCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.lblAbs = new System.Windows.Forms.Label();
+            this.lblAve = new System.Windows.Forms.Label();
+            this.lblProd = new System.Windows.Forms.Label();
+            this.lblDiff = new System.Windows.Forms.Label();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.Label8 = new System.Windows.Forms.Label();
+            this.Label7 = new System.Windows.Forms.Label();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblMin
+            // 
+            this.lblMin.BackColor = System.Drawing.Color.Coral;
+            this.lblMin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblMin.Location = new System.Drawing.Point(251, 353);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(233, 29);
+            this.lblMin.TabIndex = 39;
+            this.lblMin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMax
+            // 
+            this.lblMax.BackColor = System.Drawing.Color.Coral;
+            this.lblMax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblMax.Location = new System.Drawing.Point(251, 324);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(233, 29);
+            this.lblMax.TabIndex = 38;
+            this.lblMax.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAbs
+            // 
+            this.lblAbs.BackColor = System.Drawing.Color.Coral;
+            this.lblAbs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAbs.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblAbs.Location = new System.Drawing.Point(251, 295);
+            this.lblAbs.Name = "lblAbs";
+            this.lblAbs.Size = new System.Drawing.Size(233, 29);
+            this.lblAbs.TabIndex = 37;
+            this.lblAbs.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAve
+            // 
+            this.lblAve.BackColor = System.Drawing.Color.Coral;
+            this.lblAve.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAve.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblAve.Location = new System.Drawing.Point(251, 266);
+            this.lblAve.Name = "lblAve";
+            this.lblAve.Size = new System.Drawing.Size(233, 29);
+            this.lblAve.TabIndex = 36;
+            this.lblAve.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblProd
+            // 
+            this.lblProd.BackColor = System.Drawing.Color.Coral;
+            this.lblProd.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblProd.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblProd.Location = new System.Drawing.Point(251, 237);
+            this.lblProd.Name = "lblProd";
+            this.lblProd.Size = new System.Drawing.Size(233, 29);
+            this.lblProd.TabIndex = 35;
+            this.lblProd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDiff
+            // 
+            this.lblDiff.BackColor = System.Drawing.Color.Coral;
+            this.lblDiff.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDiff.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblDiff.Location = new System.Drawing.Point(251, 208);
+            this.lblDiff.Name = "lblDiff";
+            this.lblDiff.Size = new System.Drawing.Size(233, 29);
+            this.lblDiff.TabIndex = 34;
+            this.lblDiff.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSum
+            // 
+            this.lblSum.BackColor = System.Drawing.Color.Coral;
+            this.lblSum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblSum.Location = new System.Drawing.Point(251, 179);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(233, 29);
+            this.lblSum.TabIndex = 33;
+            this.lblSum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label8
+            // 
+            this.Label8.BackColor = System.Drawing.Color.Salmon;
+            this.Label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Label8.ForeColor = System.Drawing.Color.Black;
+            this.Label8.Location = new System.Drawing.Point(12, 353);
+            this.Label8.Name = "Label8";
+            this.Label8.Size = new System.Drawing.Size(233, 29);
+            this.Label8.TabIndex = 32;
+            this.Label8.Text = "Min:";
+            this.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label7
+            // 
+            this.Label7.BackColor = System.Drawing.Color.Salmon;
+            this.Label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Label7.ForeColor = System.Drawing.Color.Black;
+            this.Label7.Location = new System.Drawing.Point(12, 324);
+            this.Label7.Name = "Label7";
+            this.Label7.Size = new System.Drawing.Size(233, 29);
+            this.Label7.TabIndex = 31;
+            this.Label7.Text = "Max:";
+            this.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label6
+            // 
+            this.Label6.BackColor = System.Drawing.Color.Salmon;
+            this.Label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Label6.ForeColor = System.Drawing.Color.Black;
+            this.Label6.Location = new System.Drawing.Point(12, 295);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(233, 29);
+            this.Label6.TabIndex = 30;
+            this.Label6.Text = "Absolute Differance:";
+            this.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label5
+            // 
+            this.Label5.BackColor = System.Drawing.Color.Salmon;
+            this.Label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Label5.ForeColor = System.Drawing.Color.Black;
+            this.Label5.Location = new System.Drawing.Point(12, 266);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(233, 29);
+            this.Label5.TabIndex = 29;
+            this.Label5.Text = "Average:";
+            this.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label4
+            // 
+            this.Label4.BackColor = System.Drawing.Color.Salmon;
+            this.Label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Label4.ForeColor = System.Drawing.Color.Black;
+            this.Label4.Location = new System.Drawing.Point(12, 237);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(233, 29);
+            this.Label4.TabIndex = 28;
+            this.Label4.Text = "Product";
+            this.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label3
+            // 
+            this.Label3.BackColor = System.Drawing.Color.Salmon;
+            this.Label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Label3.ForeColor = System.Drawing.Color.Black;
+            this.Label3.Location = new System.Drawing.Point(12, 208);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(233, 29);
+            this.Label3.TabIndex = 27;
+            this.Label3.Text = "Difference:";
+            this.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label2
+            // 
+            this.Label2.BackColor = System.Drawing.Color.Salmon;
+            this.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Label2.ForeColor = System.Drawing.Color.Black;
+            this.Label2.Location = new System.Drawing.Point(12, 179);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(233, 29);
+            this.Label2.TabIndex = 26;
+            this.Label2.Text = "Sum:";
+            this.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label1
+            // 
+            this.Label1.BackColor = System.Drawing.Color.Salmon;
+            this.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Label1.Location = new System.Drawing.Point(12, 19);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(233, 29);
+            this.Label1.TabIndex = 25;
+            this.Label1.Text = "Enter 2 Numbers:";
+            this.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Button3
+            // 
+            this.Button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Button3.Location = new System.Drawing.Point(251, 121);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(233, 52);
+            this.Button3.TabIndex = 24;
+            this.Button3.Text = "Exit";
+            this.Button3.UseVisualStyleBackColor = true;
+            // 
+            // Button2
+            // 
+            this.Button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Button2.Location = new System.Drawing.Point(251, 63);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(233, 52);
+            this.Button2.TabIndex = 23;
+            this.Button2.Text = "Clear";
+            this.Button2.UseVisualStyleBackColor = true;
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.TextBox2.Location = new System.Drawing.Point(12, 132);
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(233, 32);
+            this.TextBox2.TabIndex = 22;
+            // 
+            // Button1
+            // 
+            this.Button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.Button1.Location = new System.Drawing.Point(251, 5);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(233, 52);
+            this.Button1.TabIndex = 21;
+            this.Button1.Text = "Calculate";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.TextBox1.Location = new System.Drawing.Point(12, 74);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(233, 32);
+            this.TextBox1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(498, 388);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblAbs);
+            this.Controls.Add(this.lblAve);
+            this.Controls.Add(this.lblProd);
+            this.Controls.Add(this.lblDiff);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.Label8);
+            this.Controls.Add(this.Label7);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.Button3);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.TextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label lblMin;
+        internal System.Windows.Forms.Label lblMax;
+        internal System.Windows.Forms.Label lblAbs;
+        internal System.Windows.Forms.Label lblAve;
+        internal System.Windows.Forms.Label lblProd;
+        internal System.Windows.Forms.Label lblDiff;
+        internal System.Windows.Forms.Label lblSum;
+        internal System.Windows.Forms.Label Label8;
+        internal System.Windows.Forms.Label Label7;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Button Button3;
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.TextBox TextBox2;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.TextBox TextBox1;
+    }
+}
+
