81569c84ef1e20c39d285f76fe6a210753442b29
GuernseyL <154453720+GuernseyL@users.noreply.github.com>
2024-04-24 14:39:31 -0500

    Update 4/24/2024

---
+++ CSharp/Pg273Bookpoints/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg273Bookpoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of books bought this month: ");
+            int Books = int.Parse(Console.ReadLine());
+            if (Books == 0) Console.WriteLine("You earn 0 BookPoints");
+            else if (Books == 1) Console.WriteLine("You earn 5 BookPoints");
+            else if (Books == 2) Console.WriteLine("You earn 15 BookPoints");
+            else if (Books == 3) Console.WriteLine("You earn 30 BookPoints");
+            else if (Books >= 4) Console.WriteLine("You earn 60 BookPoints");
+            else Console.WriteLine("You haven't bought any books");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/pg273MassandWeight/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg273MassandWeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the mass in kilograms: ");
+            double Mass = double.Parse(Console.ReadLine());
+            double Newtons = Mass * 9.8;
+            Console.WriteLine("The weight of the item is " + Math.Round(Newtons, 2) + " newtons");
+            Console.ReadLine();
+        }
+    }
+}
