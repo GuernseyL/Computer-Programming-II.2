4dc4333e1e694bb44b226d72bc386d029f5dd3af
GuernseyL <154453720+GuernseyL@users.noreply.github.com>
2024-05-15 14:39:13 -0500

    update 5/15/2024

---
+++ CSharp/LiveChat/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int X = 0;
+while (X > -1)
+{
+    Console.Write("");
+    string Message = Console.ReadLine();
+    Console.WriteLine(Message);
+}
+Console.ReadLine();
