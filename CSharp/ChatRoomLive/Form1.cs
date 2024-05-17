e1f1e1b8d6d9388a0e93d7550fa2f987fe0502c0
GuernseyL <154453720+GuernseyL@users.noreply.github.com>
2024-05-17 14:36:28 -0500

    Update 5/17/2024

--- CSharp/ChatRoomLive/Form1.cs
+++ CSharp/ChatRoomLive/Form1.cs
@@ -7,20 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.VisualBasic;
 
-namespace LiveChatRoom
+namespace ChatRoomLive
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string ServerName = Interaction.inputbox("Enter server name", "Server name here:");
         }
     }
 }
