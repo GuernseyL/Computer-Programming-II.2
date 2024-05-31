374fb952eacfbaf7ffffbc946ce19742dde1944e
GuernseyL <154453720+GuernseyL@users.noreply.github.com>
2024-05-31 14:35:47 -0500

    Update 5/31/2024

--- CSharp/LiveChat/Program.cs
+++ CSharp/LiveChat/Program.cs
@@ -2,9 +2,20 @@
 {
     Console.WriteLine("Host or join a server. (Host/Join)");
     string HostClient = Console.ReadLine();
-    if (HostClient == "Host") { Console.WriteLine("Enter your admin name"); string HostUser = Console.ReadLine(); Console.WriteLine(HostUser); }
-    else if (HostClient == "Join") { Console.WriteLine("Enter your guest name"); string GuestUser = Console.ReadLine(); Console.WriteLine(GuestUser); }
+    string HostUser = " ";
+    string GuestUser = " ";
+    if (HostClient == "Host") { Console.WriteLine("Enter your admin name"); HostUser = Console.ReadLine(); Console.WriteLine(HostUser); }
+    else if (HostClient == "Join") { Console.WriteLine("Enter your guest name"); GuestUser = Console.ReadLine(); Console.WriteLine(GuestUser); }
     else { Console.WriteLine("You did not enter Host or Join, shutting down."); System.Environment.Exit(0); }
     Console.WriteLine(HostClient);
-    Console.ReadLine();
+    Console.WriteLine("Enter your message (Type 'Room.Exit' to leave:");
+    int constant = 0;
+    while (constant <= 0)
+    {
+        string Message = Console.ReadLine(); Console.WriteLine("{" + HostUser + "}  " + "'" + Message + "'"); if (Message == "Room.Exit") { constant++; }
+    }
+//Create host server
+//Allow client server joining
+//User message system
+
 }
